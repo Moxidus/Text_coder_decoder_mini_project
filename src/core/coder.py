4bfc3d3cb2c951188c033758e18288023a15cf35
Moxidus <dominikkutynsky@gmail.com>
2026-04-25 19:57:04 +0100

    added placeholder decoder and encoder

--- src/core/coder.py
+++ src/core/coder.py
@@ -1,17 +1,47 @@
 """
 coder.py
 
+Class for Data Coding (Encoding)
 
-Class for Data Coding (Encoding): class Coder
-
-o This class will implement algorithm/method to Encode (code) the input text
+-This class will implement algorithm/method to Encode (code) the input text
 using a codeword to transform it into a coded text file that is not humanreadable as plain text, but is fully reversible using the same software and the
 correct codeword. Simple character substitution alone is not sufficient.
+
+Current implementation is just ceasar cipher as a placeholder as a placeholder
+
 """
 
 
 class Coder:
+    default_shift = 5
+    def __init__(self):
+        pass
+
+    def encode(self, passkey: str, text : str) -> str:
+        text = text.lower()
+
+        encrypted_text = ""
+
+        for char in text:
+            if not char.isalpha():
+                encrypted_text += char
+                continue
+
+            ascii_val = ord(char) + self.default_shift
+            if ascii_val > ord("z"):
+                ascii_val -= 26
+            encrypted_text += chr(ascii_val)
+
+        return encrypted_text
+
+
+
+
+if __name__ == "__main__":
+    coder = Coder()
+
+    print("Start test")
+    res = coder.encode("", "hello world")
+    print("Encoded text", res)
+
     pass
-
-
-
--- src/core/decoder.py
+++ src/core/decoder.py
@@ -8,10 +8,33 @@ o The coded file must include a code verification value that is checked during
 decoding to confirm the codeword is correct.
 o If a wrong codeword is supplied, decoding must fail clearly, must not generate
 a decoded output file, and must display an appropriate error status in the UI
+
+Curretly example Ceasar cypher implemented
 """
 
 class Decoder:
-    pass
+    default_shift = 5
+    def __init__(self):
+        pass
+    
+
+    def decode(self, passkey: str, text : str) -> str:
+        text = text.lower()
+
+        encrypted_text = ""
+
+        for char in text:
+            
+            if not char.isalpha():
+                encrypted_text += char
+                continue
+
+            ascii_val = ord(char) - self.default_shift
+            if ascii_val < ord("a"):
+                ascii_val += 26
+            encrypted_text += chr(ascii_val)
+
+        return encrypted_text
 
 
 
