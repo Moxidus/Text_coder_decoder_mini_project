57627c7f4fc910addc9aa5221285da6447b01abb
Moxidus <dominikkutynsky@gmail.com>
2026-04-27 00:18:50 +0100

    spelling

---
+++ tests/test_CaeserCipher.py
@@ -0,0 +1,33 @@
+from core.CaesarCipher import caesarCipher
+
+TEST_PASSKEY = "Supper secret password"
+TEST_SHIFT = 5
+TEST_MESSAGE = "hello world"
+TEST_MESSAGE_ENCODED_1 = "mjqqt btwqi"
+TEST_MESSAGE_ENCODED_2 = "dahhk sknhz"
+
+def test_encoding():
+    coder = caesarCipher()
+    result = coder.encode(TEST_SHIFT, TEST_MESSAGE)
+
+    assert result == TEST_MESSAGE_ENCODED_1
+
+def test_decoding():
+    coder = caesarCipher()
+    result = coder.decode(TEST_SHIFT, TEST_MESSAGE_ENCODED_1)
+
+    assert result == TEST_MESSAGE
+
+def test_encoding_with_passkey():
+    coder = caesarCipher()
+    shift = coder.string_to_shift(TEST_PASSKEY)
+    result = coder.encode(shift,TEST_MESSAGE)
+
+    assert result == TEST_MESSAGE_ENCODED_2
+
+def test_decoding_with_passkey():
+    coder = caesarCipher()
+    shift = coder.string_to_shift(TEST_PASSKEY)
+    result = coder.decode(shift, TEST_MESSAGE_ENCODED_2)
+
+    assert result == TEST_MESSAGE
