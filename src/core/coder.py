1a93db032ce0c482989e8ea104bc283b9552492b
Moxidus <dominikkutynsky@gmail.com>
2026-04-27 17:41:03 +0100

    Coder supports both CaesarCipher and CaesarCipher, fixed unit tests for Coder

--- src/core/coder.py
+++ src/core/coder.py
@@ -4,44 +4,46 @@ coder.py
 Class for Data Coding (Encoding)
 
 -This class will implement algorithm/method to Encode (code) the input text
-using a codeword to transform it into a coded text file that is not humanreadable as plain text, but is fully reversible using the same software and the
+using a codeword to transform it into a coded text file that is not human readable as plain text, but is fully reversible using the same software and the
 correct codeword. Simple character substitution alone is not sufficient.
-
-Current implementation is just caesar cipher as a placeholder as a placeholder
 
 """
 
+import numpy as np
+import base64
+from enum import Enum
+from core.CaesarCipher import caesarCipher
+from core.CustomCipher import customCipher
+
+class EncryptionType(Enum):
+    CAESAR_CIPHER = 1
+    CUSTOM_CIPHER = 2
+
 
 class Coder:
-    default_shift = 5
     def __init__(self):
-        pass
+        self.caesarCoder = caesarCipher()
+        self.customCoder = customCipher()
 
-    def encode(self, passkey: str, text : str) -> str:
-        text = text.lower()
+    def encode(self, passkey: str, text : str, encryption_type: EncryptionType = EncryptionType.CUSTOM_CIPHER, custom_salt: str = None) -> str:
+        if encryption_type == EncryptionType.CAESAR_CIPHER:
+            shift = self.caesarCoder.string_to_shift(passkey)
+            result = self.caesarCoder.encode(shift, text)
+            return result
+        else:
+            result = self.customCoder.encode(passkey, text, custom_salt)
+            return result
 
-        encrypted_text = ""
+    def decode(self, passkey: str, encrypted_base64, encryption_type: EncryptionType = EncryptionType.CUSTOM_CIPHER) -> str:
 
-        for char in text:
-            if not char.isalpha():
-                encrypted_text += char
-                continue
-
-            ascii_val = ord(char) + self.default_shift
-            if ascii_val > ord("z"):
-                ascii_val -= 26
-            encrypted_text += chr(ascii_val)
-
-        return encrypted_text
-
-
+        if encryption_type == EncryptionType.CAESAR_CIPHER:
+            shift = self.caesarCoder.string_to_shift(passkey)
+            result = self.caesarCoder.decode(shift, encrypted_base64)
+            return result
+        else:
+            result = self.customCoder.decode(passkey, encrypted_base64, None)
+            return result
 
 
 if __name__ == "__main__":
     coder = Coder()
-
-    print("Start test")
-    res = coder.encode("", "hello world")
-    print("Encoded text", res)
-
-    pass
--- tests/test_Encoder.py
+++ tests/test_Encoder.py
@@ -1,8 +1,14 @@
 
-from core.coder import Coder
+from core.coder import Coder, EncryptionType
 
-def test_encoding():
+def test_encoding_caesar():
     coder = Coder()
-    result = coder.encode("","hello world")
+    result = coder.encode("password 1","hello world", EncryptionType.CAESAR_CIPHER)
 
-    assert result == "mjqqt btwqi"
+    assert result == "jgnnq yqtnf"
+
+def test_encoding_custom():
+    coder = Coder()
+    result = coder.encode("password 1","hello world", EncryptionType.CUSTOM_CIPHER, custom_salt="abcdef") # TODO: need a way to force constant salt
+
+    assert result == 'qiAgG10TXfvzJd5oliiRWWyZ3v8hnYgdSGzgGrhzmSexje1KKc8qiB76WENHCTjEct/XbiHWZfUvPGiui12n4FN7l0zsh9ijabcdef'
